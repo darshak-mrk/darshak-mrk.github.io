cd9109e60a300b96acc3ac350c927eb99d516ce6
darshak-mrk <108942038+darshak-mrk@users.noreply.github.com>
2022-07-08 09:49:32 -0700

    Delete cylinder.js

--- cylinder.js
+++
@@ -1,7 +0,0 @@
-function volume (){
-	var v1 = document.getElementById('value1').value;
-	var v2 = document.getElementById('value2').value;
-	var volume = 3.14 * parseInt(v1) * parseInt(v1) * parseInt(v2);
-	document.getElementById('value3').value = volume;
-	
-}
